633c046d0d72b650380086330e4083b82e44e586
psb-david-petty <psb-david-petty@users.noreply.github.com>
2020-07-14 18:58:51 -0400

    Updated comments to accurately reflect behaviors.

--- src/logicproof.py
+++ src/logicproof.py
@@ -3,15 +3,25 @@
 #
 # logicproof.py
 #
+# TODO: --verbose only allows for two
+#
 """
-logicproof.py takes one or more premises and compares them with the goal.
-The premises and the goal must be in postfix form.
+logicproof.py takes zero or more premises and compares them with a goal.
+
+http://intrologic.stanford.edu/glossary/operator_precedence.html
+
+The premises and the goal must be in postfix form with tokens separated by
+spaces to ease parsing, for example:
+
+    a & b | c & d => e                  # which would be parenthesized as:
+    ((a & b) | (c & d)) => e            # is written in postfix form as:
+    a b & c d & | e =>
 """
 
 __author__ = "David C. Petty"
 __copyright__ = "Copyright 2020, David C. Petty"
 __license__ = "https://creativecommons.org/licenses/by-nc-sa/4.0/"
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 __maintainer__ = "David C. Petty"
 __email__ = "david_petty@psbma.org"
 __status__ = "Hack"
@@ -21,7 +31,9 @@ import argparse, os, re, sys
 
 class Table:
     """Generate and print truth table for premises and goal."""
-    _sep = '*'                              # separator
+    _sep = '*'                              # table separator
+    _emp = '+'                              # word emphasis
+    _empty = ['T']                          # default empty premises
     _top = (0, lambda: 1)                   # true
     _nop = (1, lambda o: ~o & 1)            # not
     _aop = (2, lambda a, b: a & b)          # and
@@ -29,93 +41,102 @@ class Table:
     _iop = (2, lambda a, b: (~a | b) & 1)   # inference
     _bop = (2, lambda a, b: ~(a ^ b) & 1)   # biconditional
     # _ops is the dictionary of valid operation tuples.
-    _ops = \
-        {'T': _top,
-         '~': _nop,
-         '&': _aop, '|': _oop, '=>': _iop, '<=>': _bop, 'AND': _aop}
+    _ops = {'T': _top,                      # arity 0
+            '~': _nop,                      # arity 1, arity 2
+            '&': _aop, '|': _oop, '=>': _iop, '<=>': _bop, 'AND': _aop}
 
     def __init__(self, premises, goal, verbose=False):
-        """Generate and print truth table for premises and goal."""
-        self._premises = self.parse(premises)
-        self._goals = self.parse(goal)
+        """Generate truth table for premises and goal."""
+        self._premises = self._parse(premises)
+        self._goals = self._parse(goal)
         assert len(self._goals) == 1, f"goal ({self._goals}) has > 1 item"
         self._verbose = verbose
-        self._vars = set([t.lower() for exp in self._premises + self._goals
-                          for t in exp.split(' ')
-                          if t.lower() in 'abcdefghijklmnopqrstuvwxyz'])
+        # Collect single-letter variable names from premises and goal, unless
+        # premises is _empty.
+        self._vars = set([t.lower() for exp in (
+            self._premises if self._premises != self._empty else list())
+            + self._goals for t in exp.split(' ')
+            if t.lower() in 'abcdefghijklmnopqrstuvwxyz'])
         self._table = self._generate()
+
+    """ ############### Table properties ############### """
 
     @property
     def premises(self):
-        """Return self._premises property."""
+        """Return self._premises list property."""
         return self._premises
 
     @property
     def goals(self):
-        """Return self._goals property."""
+        """Return self._goals list property."""
         return self._goals
 
     @property
     def table(self):
-        """Return self._table property."""
+        """Return self._table multi-line string property."""
         return '\n'.join(self._table)
 
     @property
     def vars(self):
-        """Return self._vars property."""
+        """Return self._vars set property."""
         return self._vars
 
     @property
     def verbose(self):
-        """Return self._verbose property."""
+        """Return self._verbose Boolean property."""
         return self._verbose
 
+    """ ################ static methods ################ """
+
     @staticmethod
-    def parse(lines):
-        """Return list of lines stripped of comments."""
+    def _parse(lines):
+        """Return list of non-empty lines stripped of '#' comments."""
         return [
             (s[: s.find('#')] if s.find('#') >= 0 else s).strip()
             for s in lines.split('\n') if s
         ]
 
     @staticmethod
-    def values(n, var):
+    def _values(n, var):
         """Return value dict for reversed sorted var list.
-        For example 3 might yield {'a': 0, 'b': 1, 'c': '1'}"""
+        For example, an n of 3 might yield {'a': 0, 'b': 1, 'c': '1'}"""
         assert 0 <= n < 2 ** len(var), f"{n} not on [0, 2 ** len({var}))"
         result = dict()
         for i, v in enumerate(reversed(sorted(var))):
             result[v] = (1 << i & n) >> i
         return result
 
-    """ ############### Create properties ############## """
+    """ ############### instance methods ############### """
 
     def _evaluate_premises(self, n):
-        """Return ..."""
+        """Return _evaluate of AND-conjoined premises for vars encoded w/ n."""
         return self._evaluate(
             ' '.join(self.premises + ['AND'] * (len(self.premises) - 1)),
-            self.values(n, self.vars))
+            self._values(n, self.vars))
 
     def _evaluate_goal(self, n):
-        """Return ..."""
-        return self._evaluate(self.goals[0], self.values(n, self.vars))
+        """Return _evaluate of one-and-only goal for vars encoded w/ n"""
+        return self._evaluate(self.goals[0], self._values(n, self.vars))
 
     def _markdown_encode(self, s):
-        """Encode logic expressions and markdown table."""
+        """Encode logic expressions for markdown table. In particular, the
+        discunction symbol '|' is the same as the table cell delimiter."""
         an, aa, ao, ai, ab = '~', '&', '|', '=>', '<=>'
         un, ua, uo, ui, ub = '&not;', '&and;', '&or;', '&rArr;', '&hArr;'
         un, ua, uo, ui, ub = '\u00AC', '\u2227', '\u2228', '\u21D2', '\u21D4'
-        # ab must be replaced before ai and self._sep must be replaced last.
+        ms, me = ' | ', '**'
+        # ab must be replaced before ai, self._sep must be replaced after ao,
+        # and self._emp must be replaced last.
         return s.replace(an, un)\
             .replace(aa, ua) \
             .replace(ao, uo) \
             .replace(ab, ub) \
             .replace(ai, ui) \
-            .replace(self._sep, ' | ') \
-            .replace('+', '**').strip() # then format '+' for bold
+            .replace(self._sep, ms) \
+            .replace(self._emp, me).strip()
 
     def _header(self):
-        """Create header for table."""
+        """Create two-line header for table."""
         dvb, nl, sep, wsq = '\u2016', '\n', self._sep, '\u25a1'
         vnames = f"{''.join(sorted(self.vars))}"
         pexp, pval = zip(*self._evaluate_premises(0))
@@ -129,7 +150,7 @@ class Table:
     def _line(self, n):
         """Format table line."""
         dvb, sep, wsq = '\u2016', self._sep, '\u25a1'
-        values, b = self.values(n, self.vars), lambda n: 'T' if n else 'F'
+        values, b = self._values(n, self.vars), lambda n: 'T' if n else 'F'
         pexp, pval = zip(*self._evaluate_premises(n))
         gexp, gval = zip(*self._evaluate_goal(n))
         number = \
@@ -137,6 +158,7 @@ class Table:
         line = self._markdown_encode(
             f"{sep}{number}{sep}{dvb}{sep}{sep.join(b(v) for v in pval)}{sep}" \
             f"{wsq}{sep}{sep.join(b(v) for v in gval)}{sep}")
+        # TODO:
         if pval[-1]:
             return line.replace(wsq, '\u25a0')
         elif self.verbose:
@@ -157,8 +179,8 @@ class Table:
                 num, op = self._ops[t]  #
                 pops = stack[-num: ]    #
                 del stack[-num: ]       #
-                exps, vals = list(pop[0] for pop in pops), list(pop[1] for pop in pops)
-                exp, val = ' '.join(exps + [t]), op(*vals)
+                exps, vals = zip(*pops) if pops else (list(), list(), )
+                exp, val = ' '.join(list(exps) + [t]), op(*vals)
                 stack.append((exp, val, ))
                 calcs.append((exp, val,))
             else:
@@ -189,7 +211,7 @@ class Parser(argparse.ArgumentParser):
 
 
 def main(argv):
-    """Collect command-line arguments and call  """
+    """Collect command-line arguments, create Table, and print it."""
     description = """Collect one or more premises and a goal and display a 
     truth table whose entries are every combination of inputs where all 
     premises are True."""
@@ -203,7 +225,6 @@ def main(argv):
          'echo premise(s) and goal',),
         ('-v', '--verbose', 'store_true', 'VERBOSE', False,
          'echo entire truth table',),
-
     ]
     # Add optional arguments with values.
     for c1, c2, a, v, d, h in arguments:
@@ -214,7 +235,7 @@ def main(argv):
     # Parse arguments.
     pa = parser.parse_args(args=argv[1: ])
     if not pa.PREMISE:
-        pa.PREMISE = ['T']
+        pa.PREMISE = Table._empty
     if pa.ECHO:
         if pa.PREMISE:
             space, sq = '\n' + 11 * ' ', "'"
